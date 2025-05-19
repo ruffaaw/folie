72b7a691b6d4e4a1c1e6c7b3ad0ee903d7240d28
ruffaaw <rafalciupek07@gmail.com>
2025-05-19 15:17:00 +0200

    feat: added header

---
+++ components/Header.tsx
@@ -0,0 +1,132 @@
+"use client";
+import { ChevronDown } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useRef, useEffect } from "react";
+export default function Header() {
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const submenuRef = useRef<HTMLDivElement>(null);
+
+  const handleMouseEnter = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    setIsSubmenuOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setIsSubmenuOpen(false);
+    }, 100);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <header className="fixed top-0 w-full bg-blue flex justify-between items-center px-32 py-3">
+      <Link href="/">
+        <Image
+          src="/logo.jpg"
+          alt="logo"
+          width={80}
+          height={80}
+          className="hover:scale-125"
+        />
+      </Link>
+
+      <div className="flex flex-row gap-12 text-light text-2xl drop-shadow-[2px_2px_4px_rgba(0,0,0,0.5)]">
+        <Link href="/o-nas" className="hover:scale-125">
+          O nas
+        </Link>
+        <div
+          className="relative"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          ref={submenuRef}
+        >
+          <div
+            className={`flex items-center gap-1 cursor-pointer ${
+              isSubmenuOpen ? "scale-125" : "scale-100"
+            }`}
+          >
+            <Link href="/folie-okienne">Folie okienne</Link>
+            <ChevronDown
+              className={`transition-transform ${
+                isSubmenuOpen ? "rotate-180" : ""
+              }`}
+            />
+          </div>
+
+          {isSubmenuOpen && (
+            <div className="absolute top-full -left-[67px] mt-2 w-76 bg-white text-dark rounded-md shadow-lg z-50">
+              <Link
+                href="/folie-okienne/folie-anty-uv"
+                className="block px-4 py-2 hover:bg-blue-light hover:text-blue rounded-t-md border-b border-blue"
+              >
+                Folie Anty UV
+              </Link>
+              <Link
+                href="/folie-okienne/folie-przeciwsloneczne"
+                className="block px-4 py-2 hover:bg-blue-light hover:text-blue border-b border-blue"
+              >
+                Folie przeciwsłoneczne
+              </Link>
+              <Link
+                href="/folie-okienne/folie-przeciwsloneczne-na-swietliki-z-poliweglanu"
+                className="block px-4 py-2 hover:bg-blue-light hover:text-blue border-b border-blue"
+              >
+                Folie przeciwsłoneczne na świetliki z poliwęglanu
+              </Link>
+              <Link
+                href="/folie-okienne/folie-antywlamaniowe"
+                className="block px-4 py-2 hover:bg-blue-light hover:text-blue border-b border-blue"
+              >
+                Folie antywłamaniowe
+              </Link>
+              <Link
+                href="/folie-okienne/folie-bezpieczne"
+                className="block px-4 py-2 hover:bg-blue-light hover:text-blue border-b border-blue"
+              >
+                Folie bezpieczne
+              </Link>
+              <Link
+                href="/folie-okienne/folie-lustro-weneckie"
+                className="block px-4 py-2 hover:bg-blue-light hover:text-blue border-b border-blue"
+              >
+                Folie lustro weneckie
+              </Link>
+              <Link
+                href="/folie-okienne/folie-matowe"
+                className="block px-4 py-2 hover:bg-blue-light hover:text-blue border-b border-blue"
+              >
+                Folie matowe
+              </Link>
+              <Link
+                href="/folie-okienne/folie-mrozone"
+                className="block px-4 py-2 hover:bg-blue-light hover:text-blue rounded-b-md"
+              >
+                Folie mrożone
+              </Link>
+            </div>
+          )}
+        </div>
+        <Link href="/montaz-i-wycena" className="hover:scale-125">
+          Montaż i wycena
+        </Link>
+        <Link href="/nasze-realizacje" className="hover:scale-125">
+          Nasze realizacje
+        </Link>
+        <Link href="/kontakt" className="hover:scale-125">
+          Kontakt
+        </Link>
+      </div>
+    </header>
+  );
+}
