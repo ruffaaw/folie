db7088da7fc796184c59f74a453b712389582493
ruffaaw <rafalciupek07@gmail.com>
2025-05-28 14:54:53 +0200

    feat: added data to "folie anty uv"

---
+++ data/folie-anty-uv.tsx
@@ -0,0 +1,32 @@
+export const negative = [
+  {
+    title: "Przedwczesne starzenie skóry",
+    description:
+      "Promienie UV niszczą kolagen, prowadząc do utraty elastyczności i głębokich zmarszczek.",
+  },
+  {
+    title: "Przebarwienia i plamy",
+    description:
+      "Długotrwała ekspozycja powoduje nierównomierną pigmentację i plamy posłoneczne.",
+  },
+  {
+    title: "Ryzyko nowotworów skóry",
+    description:
+      "Zwiększone prawdopodobieństwo czerniaka i innych nowotworów skóry.",
+  },
+  {
+    title: "Oparzenia słoneczne",
+    description:
+      "Nawet przez szyby! UVB powoduje bolesne poparzenia i uszkodzenia naskórka.",
+  },
+  {
+    title: "Osłabienie odporności",
+    description:
+      "UV hamuje aktywność komórek Langerhansa, osłabiając lokalną odporność skóry.",
+  },
+  {
+    title: "Problemy ze wzrokiem",
+    description:
+      "Może prowadzić do zaćmy i zwyrodnienia plamki żółtej przy długiej ekspozycji.",
+  },
+];
