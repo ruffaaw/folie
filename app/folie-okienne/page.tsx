539d1373e65676f511cefe155e92c735100a8a6d
ruffaaw <rafalciupek07@gmail.com>
2025-05-28 12:33:23 +0200

    feat: updated folie-okienne

--- app/folie-okienne/page.tsx
+++ app/folie-okienne/page.tsx
@@ -23,12 +23,9 @@ export default function Page() {
         {offers.map((offer, index) => (
           <div
             key={index}
-            className="bg-white drop-shadow-[2px_2px_4px_rgba(0,0,0,0.3)] rounded-[12px]"
+            className="bg-white drop-shadow-[2px_2px_4px_rgba(0,0,0,0.3)] rounded-[12px] flex flex-col"
           >
-            <div
-              className="w-full h-auto relative"
-              style={{ aspectRatio: "390/244" }}
-            >
+            <div className="w-full relative" style={{ aspectRatio: "390/244" }}>
               <Image
                 src={offer.image}
                 alt={offer.title}
@@ -37,56 +34,46 @@ export default function Page() {
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
               />
             </div>
-            <div className="flex flex-col p-6 gap-2 items-stretch">
-              <h3 style={{ fontSize: "1.5rem", textAlign: "justify" }}>
-                {offer.title}
-              </h3>
-              <p
-                style={{
-                  fontSize: "1rem",
-                  fontWeight: "500",
-                  textAlign: "justify",
-                }}
-              >
-                {offer.description}
-              </p>
-              <div className="gap-2 flex flex-wrap">
-                {offer.advantages.map((advantage, advIndex) => (
-                  <div
-                    key={advIndex}
-                    className="flex items-center py-1 px-2.5 bg-blue-light rounded-full "
-                  >
-                    <p
-                      style={{
-                        fontSize: "0.8rem",
-                        color: "#003d66",
-                      }}
+            <div className="flex flex-col p-6 gap-2 items-stretch h-full justify-between">
+              <div>
+                <h3 style={{ fontSize: "1.5rem" }}>{offer.title}</h3>
+                <p
+                  style={{
+                    fontSize: "1rem",
+                    fontWeight: "500",
+                    textAlign: "justify",
+                  }}
+                >
+                  {offer.description}
+                </p>
+                <div className="gap-2 flex flex-wrap mt-2">
+                  {offer.advantages.map((advantage, advIndex) => (
+                    <div
+                      key={advIndex}
+                      className="flex items-center py-1 px-2.5 bg-blue-light rounded-full"
                     >
-                      {advantage}
-                    </p>
-                  </div>
-                ))}
-              </div>
-              <div className="gap-2 flex flex-wrap">
-                {offer.places.map((place, placeIndex) => (
-                  <div
-                    key={placeIndex}
-                    className="flex items-center py-1 px-2.5 bg-gray rounded-full "
-                  >
-                    <p
-                      style={{
-                        fontSize: "0.8rem",
-                        color: "#fff",
-                      }}
+                      <p style={{ fontSize: "0.8rem", color: "#003d66" }}>
+                        {advantage}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+                <div className="gap-2 flex flex-wrap mt-2">
+                  {offer.places.map((place, placeIndex) => (
+                    <div
+                      key={placeIndex}
+                      className="flex items-center py-1 px-2.5 bg-gray rounded-full"
                     >
-                      {place}
-                    </p>
-                  </div>
-                ))}
+                      <p style={{ fontSize: "0.8rem", color: "#fff" }}>
+                        {place}
+                      </p>
+                    </div>
+                  ))}
+                </div>
               </div>
               <Link
                 href={offer.href}
-                className="flex items-center justify-start gap-1 text-blue mt-2 w-fit hover:scale-110"
+                className="flex items-center gap-1 text-blue w-fit hover:scale-110 mt-4"
               >
                 Zobacz szczegóły <ArrowRight />
               </Link>
