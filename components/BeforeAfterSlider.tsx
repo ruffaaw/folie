1a8341b6b55c9542646d083c2e1aae36088027d9
ruffaaw <rafalciupek07@gmail.com>
2025-05-30 10:12:43 +0200

    feat: created before after slider

---
+++ components/BeforeAfterSlider.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+export default function BeforeAfterSlider({
+  before = "",
+  altBefore = "",
+  after = "",
+  altAfter = "",
+  sliderPos = 50,
+}: {
+  before?: string;
+  altBefore?: string;
+  after?: string;
+  altAfter?: string;
+  sliderPos?: number;
+}) {
+  const [sliderPosition, setSliderPosition] = useState(sliderPos);
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleMove = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!isDragging) return;
+
+    const rect = event.currentTarget.getBoundingClientRect();
+    const x = Math.max(0, Math.min(event.clientX - rect.left, rect.width));
+    const percent = Math.max(0, Math.min((x / rect.width) * 100, 100));
+
+    setSliderPosition(percent);
+  };
+
+  const handleMouseDown = () => {
+    setIsDragging(true);
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  return (
+    <div className="w-full relative px-32 mt-7" onMouseUp={handleMouseUp}>
+      <div
+        className="relative w-full  aspect-[390/200] m-auto overflow-hidden select-none"
+        onMouseMove={handleMove}
+        onMouseDown={handleMouseDown}
+      >
+        <Image alt={altAfter} src={after} fill draggable={false} priority />
+
+        <div
+          className="absolute top-0 left-0 right-0 w-full aspect-[390/200] m-auto overflow-hidden select-none"
+          style={{ clipPath: `inset(0 ${100 - sliderPosition}% 0 0)` }}
+        >
+          <Image fill priority draggable={false} src={before} alt={altBefore} />
+        </div>
+        <div
+          className="absolute top-0 bottom-0 w-1 bg-blue-light cursor-ew-resize"
+          style={{
+            left: `calc(${sliderPosition}% - 1px)`,
+          }}
+        >
+          <div className="bg-blue-light absolute rounded-full h-3 w-3 -left-1 top-[calc(50%-5px)]" />
+        </div>
+      </div>
+    </div>
+  );
+}
