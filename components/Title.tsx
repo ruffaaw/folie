de8dc40cc43ca084ce15953f459a4662db774499
ruffaaw <rafalciupek07@gmail.com>
2025-06-01 14:06:57 +0200

    style: added text-center

--- components/Title.tsx
+++ components/Title.tsx
@@ -12,7 +12,7 @@ export default function Title({
         <div className="w-3 h-3 bg-dark rounded-full" />
       </div>
       <div className="flex items-center justify-center px-6 py-2 border border-dark rounded-full">
-        <h5 className="text-dark text-xl">{title}</h5>
+        <h5 className="text-dark text-xl text-center">{title}</h5>
       </div>
       <div className="relative flex items-center">
         <div className="w-3 h-3 bg-dark rounded-full" />
