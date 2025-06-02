7dcacf47d245826b65cd1e9e118478c6ae90a3e7
ruffaaw <rafalciupek07@gmail.com>
2025-06-02 12:13:49 +0200

    feat: corrected displaying images

--- components/BeforeAfterSlider.tsx
+++ components/BeforeAfterSlider.tsx
@@ -50,13 +50,13 @@ export default function BeforeAfterSlider({
 
   return (
     <div
-      className="w-full relative px-4 sm:px-8 md:px-16 lg:px-32 mt-6 md:mt-8"
+      className="w-full relative md:px-16 lg:px-32 mt-6 md:mt-8"
       onMouseUp={handleEnd}
       onMouseLeave={handleEnd}
     >
       <div
         ref={containerRef}
-        className="relative w-full aspect-[390/200] m-auto overflow-hidden select-none touch-none"
+        className="relative w-full aspect-[10/7] m-auto overflow-hidden select-none touch-none"
         onMouseMove={handleMouseMove}
         onMouseDown={handleStart}
         onTouchStart={handleStart}
@@ -71,10 +71,11 @@ export default function BeforeAfterSlider({
           priority
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
           className="object-cover"
+          unoptimized
         />
 
         <div
-          className="absolute top-0 left-0 right-0 w-full aspect-[390/200] m-auto overflow-hidden select-none"
+          className="absolute top-0 left-0 right-0 w-full aspect-[10/7] m-auto overflow-hidden select-none"
           style={{ clipPath: `inset(0 ${100 - sliderPosition}% 0 0)` }}
         >
           <Image
@@ -84,6 +85,7 @@ export default function BeforeAfterSlider({
             src={before}
             alt={altBefore}
             className="object-cover"
+            unoptimized
           />
         </div>
         <div
