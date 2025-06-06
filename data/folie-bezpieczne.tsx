44b39bf7ddfe4d8956c4b2bc98f2dd3ef96adb34
ruffaaw <rafalciupek07@gmail.com>
2025-06-06 19:08:28 +0200

    feat: updated page and data

--- data/folie-bezpieczne.tsx
+++ data/folie-bezpieczne.tsx
@@ -1,43 +1,44 @@
-import {
-  Shield,
-  Home,
-  Utensils,
-  Building,
-  Users,
-  AlertTriangle,
-  Zap,
-  EyeClosed,
-  ShieldCheck,
-} from "lucide-react";
+import AirportIcon from "@/public/icons/AirportIcon";
+import AlertIcon from "@/public/icons/AlertIcon";
+import EyeOffIcon from "@/public/icons/EyeOffIcon";
+import HomeIcon from "@/public/icons/HomeIcon";
+import RestaurantIcon from "@/public/icons/RestaurantIcon";
+import SchoolIcon from "@/public/icons/SchoolIcon";
+import ShieldCheckIcon from "@/public/icons/ShieldCheckIcon";
+import ShieldIcon from "@/public/icons/ShieldIcon";
+import ShoppingCentreIcon from "@/public/icons/ShoppingCentereIcon";
+import UsersIcon from "@/public/icons/UsersIcon";
+import UtensilsIcon from "@/public/icons/UtensilsIcon";
+import ZapIcon from "@/public/icons/ZapIcon";
 
 export const features = [
   {
-    icon: <Shield />,
+    icon: <ShieldIcon />,
     title: "Zatrzymuje odłamki",
     description: "Zapobiega rozprzestrzenianiu się stłuczonego szkła",
   },
   {
-    icon: <ShieldCheck />,
+    icon: <ShieldCheckIcon />,
     title: "Certyfikat HACCP",
     description: "Spełnia wymagania dla przemysłu spożywczego",
   },
   {
-    icon: <EyeClosed />,
+    icon: <EyeOffIcon />,
     title: "Niewidoczna ochrona",
     description: `Zachowuje przejrzystość i${"\u00A0"}estetykę szkła`,
   },
   {
-    icon: <AlertTriangle />,
+    icon: <AlertIcon />,
     title: "Ochrona przed zarysowaniami",
     description: "Zabezpiecza szklane powierzchnie przed uszkodzeniami",
   },
   {
-    icon: <Users />,
+    icon: <UsersIcon />,
     title: "Bezpieczeństwo publiczne",
     description: `Chroni przed wypadkami w${"\u00A0"}miejscach użyteczności publicznej`,
   },
   {
-    icon: <Zap />,
+    icon: <ZapIcon />,
     title: "Łatwa konserwacja",
     description: `Prosta w${"\u00A0"}czyszczeniu i${"\u00A0"}utrzymaniu w${"\u00A0"}czystości`,
   },
@@ -45,33 +46,33 @@ export const features = [
 
 export const places = [
   {
-    icon: <Home />,
+    icon: <HomeIcon />,
     title: `Domy z${"\u00A0"}dziećmi`,
     description: `Ochrona przed skaleczeniami w${"\u00A0"}przypadku stłuczenia szyb`,
   },
   {
-    icon: <Utensils />,
+    icon: <UtensilsIcon />,
     title: "Przemysł spożywczy",
     description:
       "Zgodność z HACCP - zabezpieczenie żywności przed odłamkami szkła",
   },
   {
-    icon: <Building />,
+    icon: <SchoolIcon />,
     title: "Obiekty publiczne",
     description: "Szkoły, przedszkola, urzędy - ochrona wielu osób naraz",
   },
   {
-    icon: <Utensils />,
+    icon: <RestaurantIcon />,
     title: "Restauracje",
     description: `Bezpieczeństwo gości i${"\u00A0"}spełnienie wymogów sanitarnych`,
   },
   {
-    icon: <Building />,
+    icon: <ShoppingCentreIcon />,
     title: "Obiekty handlowe",
     description: `Ochrona witryn sklepowych i${"\u00A0"}stref gastronomicznych`,
   },
   {
-    icon: <Building />,
+    icon: <AirportIcon />,
     title: "Lotniska/dworce",
     description: `Zabezpieczenie dużych przeszkleń w${"\u00A0"}miejscach publicznych`,
   },
