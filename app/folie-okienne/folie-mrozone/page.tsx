74bfe5cdc0ff5aa329e4f693d894f96826afe0a9
ruffaaw <rafalciupek07@gmail.com>
2025-06-06 20:38:04 +0200

    feat: updated page and icons

--- app/folie-okienne/folie-mrozone/page.tsx
+++ app/folie-okienne/folie-mrozone/page.tsx
@@ -5,9 +5,11 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Title from "@/components/Title";
 import { places } from "@/data/folie-mrozone";
-import { LayoutGrid, Scissors } from "lucide-react";
 import React from "react";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import ShieldCheckIcon from "@/public/icons/ShieldCheckIcon";
+import PlacesIcon from "@/public/icons/PlacesIcon";
 
 const container = {
   hidden: { opacity: 0 },
@@ -27,26 +29,63 @@ const item = {
     opacity: 1,
     transition: {
       type: "spring",
-      stiffness: 100,
-      damping: 10,
-      duration: 0.6,
+      stiffness: 80,
+      damping: 12,
     },
   },
 };
 
 const card = {
-  hidden: { y: 50, opacity: 0 },
+  hidden: { y: 40, opacity: 0, scale: 0.95 },
   visible: (i: number) => ({
     y: 0,
     opacity: 1,
+    scale: 1,
     transition: {
       delay: i * 0.15,
-      duration: 0.5,
+      type: "spring",
+      stiffness: 70,
+      damping: 12,
     },
   }),
   hover: {
-    y: -5,
-    transition: { duration: 0.3 },
+    scale: 1.05,
+    boxShadow: "0px 8px 30px rgba(0, 0, 0, 0.15)",
+    transition: {
+      duration: 0.3,
+    },
+  },
+};
+
+const image = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: 0.2,
+      duration: 0.8,
+      ease: "easeOut",
+    },
+  },
+};
+
+const headline = {
+  hidden: { opacity: 0, y: 40, scale: 0.95 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    scale: 1,
+    transition: { duration: 0.8, ease: "easeOut" },
+  },
+};
+
+const sub = {
+  hidden: { opacity: 0, x: -50 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 0.7, ease: "easeOut", delay: 0.3 },
   },
 };
 
@@ -59,12 +98,22 @@ export default function Page() {
     >
       <Header />
       <Title title="PRYWATNOŚĆ I DEKORACJA" />
-      <motion.h1 variants={item} className="mt-7 text-center">
-        FOLIE <span className="text-blue">MROŻONE</span>
+      <motion.h1 variants={headline} className="mt-7 text-center">
+        <span className="text-dark relative inline-block">
+          <span>
+            FOLIE <span className="text-blue">MROŻONE</span>
+          </span>
+          <motion.span
+            initial={{ scaleX: 0 }}
+            animate={{ scaleX: 1 }}
+            transition={{ delay: 0.2, duration: 0.8, ease: "easeOut" }}
+            className="absolute bottom-0 left-0 w-full h-2 bg-blue-light origin-left"
+          />
+        </span>
       </motion.h1>
       <motion.h2
-        variants={item}
-        className="mt-4 md:mt-7 text-center px-4 sm:px-8 md:px-16 lg:px-32"
+        variants={sub}
+        className="mt-4 md:mt-7 text-center px-4 sm:px-8 md:px-16 "
       >
         Folie mrożone, nazywane inaczej mrożonką lub szybami mlecznymi to
         specjalny materiał dający efekt mrożonej szyby podobny do odbicia
@@ -83,14 +132,17 @@ export default function Page() {
       />
       <motion.div
         variants={container}
-        className="px-4 sm:px-8 md:px-16 lg:px-32 w-full"
+        className="px-4 sm:px-8 md:px-16  w-full"
       >
         <motion.div
           variants={item}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, margin: "-100px" }}
           className="flex flex-col items-start justify-start p-5 lg:p-7 gap-5 w-full bg-blue-light rounded-3xl mt-6 md:mt-8 card"
         >
           <div className="flex flex-row items-center justify-start gap-2.5 w-full">
-            <Scissors className="w-8 h-8 md:w-10 md:h-10" />
+            <ShieldCheckIcon />
             <h3>Dlaczego warto wybrać folie mrożone?</h3>
           </div>
           <motion.div
@@ -105,7 +157,7 @@ export default function Page() {
               initial="hidden"
               whileInView="visible"
               whileHover={{ scale: 1.03 }}
-              viewport={{ once: true }}
+              viewport={{ once: true, margin: "-100px" }}
               className="flex flex-col p-5 lg:p-7 gap-3 bg-light rounded-3xl w-full"
             >
               <h4 className="font-semibold">Uniwersalne zastosowanie</h4>
@@ -121,7 +173,7 @@ export default function Page() {
               initial="hidden"
               whileInView="visible"
               whileHover={{ scale: 1.03 }}
-              viewport={{ once: true }}
+              viewport={{ once: true, margin: "-100px" }}
               className="flex flex-col p-5 lg:p-7 gap-3 bg-light rounded-3xl w-full"
             >
               <h4 className="font-semibold">Łatwy montaż i&nbsp;demontaż</h4>
@@ -135,22 +187,41 @@ export default function Page() {
       </motion.div>
 
       <motion.div
+        variants={image}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, margin: "-100px" }}
+        className="w-full relative mt-6 md:mt-8 aspect-video"
+      >
+        <Image
+          src="/mrozone.webp"
+          alt="Zdjęcie folii mrożonych"
+          fill
+          className="object-cover md:px-16"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
+          priority
+          unoptimized
+        />
+      </motion.div>
+
+      <motion.div
         variants={container}
         initial="hidden"
         whileInView="visible"
-        className="px-4 sm:px-8 md:px-16 lg:px-32 w-full"
+        viewport={{ once: true, margin: "-100px" }}
+        className="px-4 sm:px-8 md:px-16  w-full"
       >
         <motion.div
           variants={item}
-          className="flex flex-col items-start justify-start py-7 gap-5 w-full  rounded-3xl mt-6 md:mt-8"
+          className="flex flex-col items-start justify-start  gap-5 w-full  rounded-3xl mt-6 md:mt-8"
         >
           <div className="flex flex-row items-center justify-start gap-2.5 w-full">
-            <LayoutGrid className="w-8 h-8 md:w-10 md:h-10" />
+            <PlacesIcon />
             <h3>Gdzie najczęściej stosujemy folie anty-UV?</h3>
           </div>
           <motion.div
             variants={container}
-            className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-5 w-full"
+            className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-5 w-full"
           >
             {places.map((item, index) => (
               <motion.div
@@ -160,7 +231,7 @@ export default function Page() {
                 initial="hidden"
                 whileInView="visible"
                 whileHover={{ scale: 1.03 }}
-                viewport={{ once: true }}
+                viewport={{ once: true, margin: "-100px" }}
                 className="flex gap-5 p-5 lg:p-7 bg-white rounded-3xl drop-shadow-[0px_0px_4px_rgba(0,0,0,0.5)] card"
               >
                 <div className="text-blue p-4 bg-blue-lighter rounded-xl flex items-center justify-center w-16 h-16 shrink-0">
@@ -184,27 +255,58 @@ export default function Page() {
         variants={container}
         initial="hidden"
         whileInView="visible"
-        className="flex flex-col items-center justify-center mt-6 md:mt-8 w-full px-4 sm:px-8 md:px-16 lg:px-32"
+        viewport={{ once: true, margin: "-100px" }}
+        className="flex flex-col items-center justify-center mt-6 md:mt-8 w-full px-4 sm:px-8"
       >
-        <h3 className="text-center">Cena folii mrożonych z&nbsp;montażem</h3>
+        <motion.h3 variants={item} className="text-xl md:text-2xl text-center">
+          Cena folii mrożonych z&nbsp;montażem
+        </motion.h3>
+
         <motion.div
           variants={item}
-          initial="hidden"
-          whileInView="visible"
-          whileHover={{ scale: 1.03 }}
-          className="flex flex-col items-center justify-center px-6 sm:px-8 md:px-12 py-4 md:py-5 w-full max-w-2xl border-y border-t-dark mt-4"
+          className="flex flex-col md:flex-row items-center justify-between gap-6 md:gap-8 w-full max-w-4xl p-6 md:p-8 mt-4 border-y border-t-dark"
         >
-          <div className="flex flex-row items-baseline p-2.5 gap-2.5">
-            <h3>120-160 </h3>
-            <p>zł/m²</p>
-          </div>
-          <p className="text-center">
-            Cena obejmuje folię mrożoną oraz profesjonalny montaż
-          </p>
+          <motion.div
+            variants={image}
+            whileHover={{ scale: 1.03 }}
+            className="w-full md:w-1/2 aspect-[3/2] relative overflow-hidden"
+          >
+            <Image
+              src="/mrozona-rulon.webp"
+              alt="Rolka Folii Matowej"
+              fill
+              className="object-cover"
+              sizes="(max-width: 768px) 100vw, 50vw"
+              priority
+            />
+          </motion.div>
+
+          <motion.div
+            variants={item}
+            className="w-full md:w-1/2 flex flex-col justify-center"
+          >
+            <div className="flex flex-row items-baseline gap-2 justify-center">
+              <h3 className="text-2xl md:text-3xl font-bold ">120-160</h3>
+              <p>zł/m²</p>
+            </div>
+
+            <p className="text-center mt-6">
+              Cena obejmuje folię mrożoną oraz profesjonalny montaż
+            </p>
+          </motion.div>
         </motion.div>
       </motion.div>
-      <BottomContact title="Chcesz zamówić folie mrożone?" className="mt-7" />
-      <Footer />
+
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ once: true }}
+        className="w-full"
+      >
+        <BottomContact title="Chcesz zamówić folie mrożone?" className="mt-7" />
+        <Footer />
+      </motion.div>
     </motion.div>
   );
 }
--- data/folie-mrozone.tsx
+++ data/folie-mrozone.tsx
@@ -1,34 +1,39 @@
-import { BookOpen, Brush, Building, Hospital, House, User } from "lucide-react";
+import BrushIcon from "@/public/icons/BrushIcon";
+import HomeIcon from "@/public/icons/HomeIcon";
+import HospitalIcon from "@/public/icons/HospitalIcon";
+import MuseumIcon from "@/public/icons/MuseumIcon";
+import OfficeIcon from "@/public/icons/OfficeIcon";
+import UserIcon from "@/public/icons/UserIcon";
 
 export const places = [
   {
-    icon: <Building />,
+    icon: <OfficeIcon />,
     title: "Biura i urzędy",
     description:
       "Jako dekoracja lub podział przestrzeni, zachowując dostęp światła dziennego.",
   },
   {
-    icon: <Hospital />,
+    icon: <HospitalIcon />,
     title: "Szpitale i gabinety",
     description: `Na salach operacyjnych i${"\u00A0"}w${"\u00A0"}gabinetach lekarskich dla zachowania prywatności.`,
   },
   {
-    icon: <House />,
+    icon: <HomeIcon />,
     title: "Mieszkania",
     description: `Na balkonach i${"\u00A0"}tarasach - popularne rozwiązanie zapewniające prywatność.`,
   },
   {
-    icon: <Brush />,
+    icon: <BrushIcon />,
     title: "Salony piękności",
     description: `W strefach zabiegowych i${"\u00A0"}przebieralniach dla komfortu klientów.`,
   },
   {
-    icon: <User />,
+    icon: <UserIcon />,
     title: "Pomieszczenia użytkowe",
     description: `W${"\u00A0"}toaletach, szatniach i${"\u00A0"}pomieszczeniach gospodarczych.`,
   },
   {
-    icon: <BookOpen />,
+    icon: <MuseumIcon />,
     title: "Obiekty kulturalne",
     description: `W muzeach i${"\u00A0"}galeriach jako element wystawienniczy.`,
   },
