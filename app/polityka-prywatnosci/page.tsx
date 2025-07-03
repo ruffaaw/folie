2639529690451a9555905f70836a7733688db9b7
ruffaaw <rafalciupek07@gmail.com>
2025-07-03 19:39:47 +0200

    feat: completed data

--- app/polityka-prywatnosci/page.tsx
+++ app/polityka-prywatnosci/page.tsx
@@ -15,7 +15,13 @@ export default function Page() {
               1. Administrator danych osobowych
             </h3>
             <div className="flex items-start mt-4">
-              <p>Administratorem Twoich danych osobowych...</p>
+              <p>
+                Administratorem Twoich danych osobowych jest Karol Jaskórski
+                Dragon Folie z&nbsp;siedzibą w&nbsp;Wołowicach, ul. Polnych
+                Kwiatów&nbsp;9, 32-070, NIP: 9442295511, REGON: 542124248. Dane
+                kontaktowe: e-mail: dragon.folie2025@gmail.com, tel.:
+                +48&nbsp;665&nbsp;333&nbsp;625, +48&nbsp;665&nbsp;333&nbsp;771.
+              </p>
             </div>
           </div>
 
@@ -28,7 +34,7 @@ export default function Page() {
               osobowe:
             </p>
             <ul className="list-disc pl-10 space-y-1 text-lg text-gray">
-              <li>Imię i nazwisko,</li>
+              <li>Imię i&nbsp;nazwisko,</li>
               <li>Adres e-mail,</li>
               <li>Numer telefonu,</li>
               <li>Wymiary szyb,</li>
@@ -42,19 +48,19 @@ export default function Page() {
 
           <div className="mb-8">
             <h3 className="text-2xl font-semibold mb-4 flex items-center">
-              3. Cel i podstawa prawna przetwarzania danych
+              3. Cel i&nbsp;podstawa prawna przetwarzania danych
             </h3>
-            <p className="mb-4">Twoje dane osobowe przetwarzamy w celu:</p>
+            <p className="mb-4">Twoje dane osobowe przetwarzamy w&nbsp;celu:</p>
             <ul className="list-disc pl-6 space-y-2 text-lg text-gray">
               <li>
                 <strong>Odpowiedzi na Twoje zapytanie</strong> przesłane przez
-                formularz kontaktowy (art. 6 ust. 1 lit. b RODO – realizacja
-                umowy lub działania przed zawarciem umowy),
+                formularz kontaktowy (art. 6 ust. 1 lit. b&nbsp;RODO –
+                realizacja umowy lub działania przed zawarciem umowy),
               </li>
               <li>
-                <strong>Archiwizacji korespondencji</strong> w celach dowodowych
-                (art. 6 ust. 1 lit. f RODO – prawnie uzasadniony interes
-                administratora).
+                <strong>Archiwizacji korespondencji</strong> w&nbsp;celach
+                dowodowych (art. 6 ust. 1 lit. f RODO – prawnie uzasadniony
+                interes administratora).
               </li>
             </ul>
           </div>
@@ -77,9 +83,9 @@ export default function Page() {
             </h3>
             <p>
               Twoje dane mogą być przekazane podmiotom świadczącym usługi IT,
-              hostingowe lub inne, które wspierają nas w realizacji usług, przy
-              czym wszystkie te podmioty zobowiązane są do ochrony danych
-              zgodnie z RODO.
+              hostingowe lub inne, które wspierają nas w&nbsp;realizacji usług,
+              przy czym wszystkie te podmioty zobowiązane są do ochrony danych
+              zgodnie z&nbsp;RODO.
             </p>
           </div>
 
@@ -97,7 +103,7 @@ export default function Page() {
               <li>Przenoszenia danych,</li>
               <li>
                 Wniesienia skargi do Prezesa Urzędu Ochrony Danych Osobowych
-                (PUODO) w przypadku naruszenia przepisów RODO.
+                (PUODO) w&nbsp;przypadku naruszenia przepisów RODO.
               </li>
             </ul>
           </div>
@@ -107,23 +113,29 @@ export default function Page() {
               7. Kontakt
             </h3>
             <p className="mb-4">
-              W sprawach związanych z przetwarzaniem danych osobowych możesz
-              skontaktować się z nami:
+              W sprawach związanych z&nbsp;przetwarzaniem danych osobowych
+              możesz skontaktować się z&nbsp;nami:
             </p>
-            <div className="flex flex-row items-center gap-5">
-              <div className="flex items-center hover:scale-110">
+            <div className="flex flex-col md:flex-row items-start md:items-center gap-5">
+              <div className="flex items-center hover:scale-110 transition-transform duration-200">
                 <Mail className="h-5 w-5 text-blue mr-2" />
                 <a
-                  href="mailto:dragon.folie@gmail.com"
+                  href="mailto:dragon.folie2025@gmail.com"
                   className="hover:text-blue "
                 >
-                  dragon.folie@gmail.com
+                  dragon.folie2025@gmail.com{" "}
                 </a>
               </div>
-              <div className="flex items-center hover:scale-110">
+              <div className="flex items-center hover:scale-110 transition-transform duration-200">
                 <Phone className="h-5 w-5 text-blue mr-2" />
-                <a href="tel:607935863" className="hover:text-blue">
-                  607 935 863
+                <a href="tel:+48665333625" className="hover:text-blue">
+                  +48&nbsp;665&nbsp;333&nbsp;625
+                </a>
+              </div>
+              <div className="flex items-center hover:scale-110 transition-transform duration-200">
+                <Phone className="h-5 w-5 text-blue mr-2" />
+                <a href="tel:+48665333771" className="hover:text-blue">
+                  +48&nbsp;665&nbsp;333&nbsp;771
                 </a>
               </div>
             </div>
